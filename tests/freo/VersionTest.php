f65c633c21e9c4054638892259a70a04cad06cd7
Sigsign <sig@signote.cc>
2023-07-19 20:13:46 +0900

    Add first testcase!

---
+++ tests/freo/VersionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class VersionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        require_once __DIR__ . '/../../libs/freo/version.php';
+    }
+
+    public function testVersion(): void 
+    {
+        $this->assertTrue(defined('FREO_VERSION'));
+    }
+}
